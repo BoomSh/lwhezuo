8b10b32c3dcb5b053fab185604b13749f7f142ea
BoomSh <979166916@qq.com>
2017-09-30 17:01:06 +0800

    w_930_1

--- xitong/application/admin/controller/Report.php
+++ xitong/application/admin/controller/Report.php
@@ -8,7 +8,71 @@ class Report extends Common
 	 * @return [type] [description]
 	 */
     public function report_contract_list(){
+        $report = model('Report');
+        if(!empty(input("startime"))){
+            if(!empty(input("overtime"))){
+                $time= " between ".strtotime(input('startime'))." and ".strtotime(input('overtime'));
+                $this->assign("startime",input("startime"));
+                $this->assign("overtime",input("overtime"));
+            }else{
+                $time= " > ".strtotime(input('startime'));
+                $this->assign("startime",input("startime"));
+            }
+        }
+        if(!empty(input("overtime"))){
+            if(!empty(input("startime"))){
+                $time= " between ".strtotime(input('startime'))." and ".strtotime(input('overtime'));
+                $this->assign("startime",input("startime"));
+                $this->assign("overtime",input("overtime"));
+            }else{
+                $time= " < ".strtotime(input('overtime'));
+                $this->assign("overtime",input("overtime"));
+            }
+        }
+        if(!empty(input("park_id"))){
+            $park = "a.id =".input("park_id");
+            $this->assign("park_id",input("park_id"));
+        }
+        /*获取 符合条件的 报表信息*/
+        $res = $report->report_contract_list($time,$park);
+        $this->assign("res",$res);
         return $this->fetch();
+    }
+    /**
+     * 导出合同报表
+     * @return [type] [description]
+     */
+    public function report_contract_excelout(){
+        $report = model('Report');
+        if(!empty(input("startime"))){
+            if(!empty(input("overtime"))){
+                $time= " between ".strtotime(input('startime'))." and ".strtotime(input('overtime'));
+                $this->assign("startime",input("startime"));
+                $this->assign("overtime",input("overtime"));
+            }else{
+                $time= " > ".strtotime(input('startime'));
+                $this->assign("startime",input("startime"));
+            }
+        }
+        if(!empty(input("overtime"))){
+            if(!empty(input("startime"))){
+                $time= " between ".strtotime(input('startime'))." and ".strtotime(input('overtime'));
+                $this->assign("startime",input("startime"));
+                $this->assign("overtime",input("overtime"));
+            }else{
+                $time= " < ".strtotime(input('overtime'));
+                $this->assign("overtime",input("overtime"));
+            }
+        }
+        if(!empty(input("park_id"))){
+            $park = "a.id =".input("park_id");
+            $this->assign("park_id",input("park_id"));
+        }
+        /*获取 符合条件的 报表信息*/
+        $res = $report->report_contract_list($time,$park);
+        $filename = "合同报表";
+        $title = array("序号","园区","地址","新签合同","到期合同","退组合同");
+        $this->goodsExcelExport($res['list'],$title,$filename);
     }
     /**
 	 * 收支报表
@@ -44,6 +108,43 @@ class Report extends Common
         $res = $report->report_exprec_list($time,$park);
         $this->assign("res",$res);
         return $this->fetch();
+    }
+    /**
+     * 导出收支
+     * @return [type] [description]
+     */
+    public function report_exprec_excelout(){
+        $report = model('Report');
+        if(!empty(input("startime"))){
+            if(!empty(input("overtime"))){
+                $time= "and `pay_time` between ".strtotime(input('startime'))." and ".strtotime(input('overtime'));
+                $this->assign("startime",input("startime"));
+                $this->assign("overtime",input("overtime"));
+            }else{
+                $time= "and `pay_time` > ".strtotime(input('startime'));
+                $this->assign("startime",input("startime"));
+            }
+        }
+        if(!empty(input("overtime"))){
+            if(!empty(input("startime"))){
+                $time= "and `pay_time` between ".strtotime(input('startime'))." and ".strtotime(input('overtime'));
+                $this->assign("startime",input("startime"));
+                $this->assign("overtime",input("overtime"));
+            }else{
+                $time= "and `pay_time` < ".strtotime(input('overtime'));
+                $this->assign("overtime",input("overtime"));
+            }
+        }
+        if(!empty(input("park_id"))){
+            $park = "a.id =".input("park_id");
+            $this->assign("park_id",input("park_id"));
+        }
+        /*获取 符合条件的 报表信息*/
+        $res = $report->report_exprec_list($time,$park);
+        $filename = "收支报表";
+        $title = array("序号","园区","支出","收入","盈利");
+        $this->goodsExcelExport($res['list'],$title,$filename);
+
     }
     /**
 	 * 房源空置报表
--- xitong/application/admin/model/Report.php
+++ xitong/application/admin/model/Report.php
@@ -9,8 +9,36 @@ class Report extends Common
 	 * 合同报表
 	 * @return [type] [description]
 	 */
-    public function report_contract_list(){
-        
+    public function report_contract_list($time,$park){
+        $where1 = "BETWEEN unix_timestamp(now())-3600*24*30*6 AND unix_timestamp(now()) ";
+        if($time!=""){
+            $where1 = $time;
+        }
+        if($park!=""){
+            $where4 = "WHERE ".$park;
+        }
+        else{
+             $where4 = "WHERE 1";
+        }
+        $res['list'] = DB::query('SELECT  a.name,a.address,b.count1,c.count2,d.count3 FROM dino_park a LEFT  JOIN ( SELECT `park_id`, count(*) AS count1 FROM dino_contract WHERE `create_time` '.$where1.'  GROUP BY park_id ) b ON b.park_id=a.id LEFT  JOIN ( SELECT `park_id`, count(*) AS count2 FROM dino_contract WHERE UNIX_TIMESTAMP(time_end)  '.$where1.' GROUP BY park_id) c ON c.park_id=a.id LEFT  JOIN ( SELECT `park_id`, count(*) AS count3 FROM dino_contract WHERE `status` = 2   GROUP BY park_id) d ON d.park_id=a.id '.$where4.'');
+        $res['num']  = count($res['list']);
+        foreach ($res['list'] as $key => $value) {
+            $res['list'][$key]['count1']= isset($res['list'][$key]['count1'])?$res['list'][$key]['count1']:0;
+            $res['list'][$key]['count2']= isset($res['list'][$key]['count2'])?$res['list'][$key]['count2']:0;
+            $res['list'][$key]['count3']= isset($res['list'][$key]['count3'])?$res['list'][$key]['count3']:0;
+            $count1[$key] = $res['list'][$key]['count1'];
+            $count2[$key] = $res['list'][$key]['count2'];
+        }
+        $res['sum_count1'] = array_sum($count1);
+        $res['sum_count2'] = array_sum($count2);
+        $res['park'] = DB::name("park")->field("id,name")->select();
+        $arr = $this->lw_number($res['num']);
+        foreach($res['list'] as $k => $v) {
+            $temp['num'] =  $arr[$k];
+            array_unshift($res['list'][$k], $temp['num']);
+        }
+    //   p_r($res);die();
+        return $res;     
     }
     /**
 	 * 收支报表
@@ -29,18 +57,22 @@ class Report extends Common
         else{
              $where2 = "WHERE 1";
         }
-        $res['list'] = DB::query('SELECT a.id, a.name,b.income,c.expend FROM dino_park a LEFT  JOIN ( SELECT `park_id`,sum(price) income FROM `dino_incomeexpenditure` '.$where1.' GROUP BY park_id ) b ON b.park_id=a.id LEFT  JOIN ( SELECT `park_id`,sum(price) expend FROM `dino_incomeexpenditure` '.$where3.' GROUP BY park_id ) c ON c.park_id=a.id '.$where2.'');
+        $res['list'] = DB::query('SELECT  a.name,c.expend,b.income FROM dino_park a LEFT  JOIN ( SELECT `park_id`,sum(price) income FROM `dino_incomeexpenditure` '.$where1.' GROUP BY park_id ) b ON b.park_id=a.id LEFT  JOIN ( SELECT `park_id`,sum(price) expend FROM `dino_incomeexpenditure` '.$where3.' GROUP BY park_id ) c ON c.park_id=a.id '.$where2.'');
         $res['num']  = count($res['list']);
         foreach ($res['list'] as $key => $value) {
             $income[$key] = $res['list'][$key]['income'];
             $expend[$key] = $res['list'][$key]['expend'];
+            $res['list'][$key]['gain'] = $res['list'][$key]['income']-$res['list'][$key]['expend'];
+            $res['list'][$key]['income']= isset($res['list'][$key]['income'])?$res['list'][$key]['income']:0;
+            $res['list'][$key]['expend']= isset($res['list'][$key]['expend'])?$res['list'][$key]['expend']:0;
         }
         $res['sum_income'] = array_sum($income);
         $res['sum_expend'] = array_sum($expend);
         $res['park'] = DB::name("park")->field("id,name")->select();
         $arr = $this->lw_number($res['num']);
         foreach($res['list'] as $k => $v) {
-            $res['list'][$k]['num']  = $arr[$k];  
+            $temp['num'] =  $arr[$k];
+            array_unshift($res['list'][$k], $temp['num']);
         }
         return $res;
     }
